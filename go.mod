ac73ca63a6522b583116c047c550096b9527a185
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-11-27 14:05:24 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/abe-dev762/ecommerce_api
+
+go 1.25.4
+
+require github.com/go-chi/chi/v5 v5.2.3
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 
+	github.com/jackc/pgx/v5 v5.7.6 
+	golang.org/x/crypto v0.37.0 
+	golang.org/x/text v0.24.0
+)
---
+++ internal/products/handlers.go
@@ -0,0 +1,30 @@
+package products
+package products
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/abe-dev762/ecommerce_api/internal/json"
+)
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *handler {
+	return &handler{
+		service: service,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.service.ListProducts(r.Context())
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.Write(w, http.StatusOK, products)
+}
